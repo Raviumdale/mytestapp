e7cab33a135e702ab65d88cf1dd9ab9250e1dc85
Raviumdale <raviumdale98@gmail.com>
2020-12-10 19:02:38 +0530

    new version2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World from jenkins!"); 
+        System.out.println("Hello, World from jenkins!");
+       System.out.println("Another Hello from Jenkins");
+         System.out.println("Another Hello from Jenkins");
+           System.out.println("Another Hello from Jenkins");
+             System.out.println("Another Hello from Jenkins");	
     }
 }
